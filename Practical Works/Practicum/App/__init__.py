f16b0f537b0e4ab880b8cd751211ae2db3419b9d
zirael <danil.aslamov@list.ru>
2026-02-23 12:29:32 +0300

    'add_projects'

---
+++ Practical Works/Practicum/App/__init__.py
@@ -0,0 +1,3 @@
+# Импорт API ключей из secrets.py
+# secrets.py не пушится на GitHub (добавлен в .gitignore)
+from secrets import api_key
---
+++ Practical Works/Practicum/App/secrets_example.py
@@ -0,0 +1,5 @@
+# Пример файла secrets.py
+# Скопируйте этот файл в secrets.py и замените значения на свои
+
+# API ключ Яндекс.Словаря (https://dictionary.yandex.net/)
+api_key = 'YOUR_API_KEY_HERE'
